5549aebf9b54b9380239d514b620f479b4cee227
Tyler Randall <tyler7r@gmail.com>
2023-02-13 22:16:27 -0500

    Add validatePlay and checkPlayer functionality

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const Gameboard = {
     gameSquare: []
 }
 
+const getGameSquare = Gameboard.gameSquare;
 
 function Player(marker, name) {
     this.marker = marker;
@@ -14,7 +15,9 @@ const playerOne = new Player('X', 'P1');
 const playerTwo = new Player('O', 'P2');
 
 const gameFlow = () => {
-    const p1Turn = true;
+    let currentTurn = 'p1';
+
+    let gamePiece = '';
 
     const displayBoard = (array) => {
         for (let i = 0; i < array.length; i++) {
@@ -23,20 +26,32 @@ const gameFlow = () => {
         }
     }
     const checkPlayer = () => {
-
+        if (currentTurn === 'p1') {
+            gamePiece = playerOne.marker;
+        } else if (currentTurn === 'p2') {
+            gamePiece = playerTwo.marker;
+        }
     }
-    const validatePlay = () => {
-
+    const validatePlay = (e) => {
+        if (e.target.textContent == 'X' || e.target.textContent == 'O') {
+            return true
+        }
     }
+
     const winnerCheck = () => {
 
     }
     const playTurn = () => {   
         squares.forEach((box) => {
             box.addEventListener('click', (e) => {
+                checkPlayer();
                 let selectedBox = Number(e.target.id.slice(7));
-                Gameboard.gameSquare[selectedBox - 1] = playerOne.marker;
-                displayBoard(Gameboard.gameSquare);
+                if (validatePlay(e) == true) {
+                    return
+                };
+                getGameSquare[selectedBox - 1] = gamePiece;
+                if (currentTurn === 'p1' ? currentTurn = 'p2' : currentTurn = 'p1'); 
+                displayBoard(getGameSquare);
             })
         })
     }
